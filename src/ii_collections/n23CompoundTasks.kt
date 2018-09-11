c9591c5c98a39ba954f9102bd3e97093c657567f
Safira Nugroho <snugroho@thoughtworks.com>
2018-09-11 11:49:52 +1000

    Done collections module, 22-24 yay!

--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -2,17 +2,22 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return customers.filter{ it.orderedProducts.contains(product) }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    val deliveredOrders = orders.filter{ it.isDelivered }
+    val deliveredProducts = deliveredOrders.map{ it.products }.flatMap{ it.toList() }
+    return deliveredProducts.maxBy{ it.price }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    val allOrders = customers.map{ it.orders }.flatMap{ it.toList() }
+    val allOrderedProducts = allOrders.map { it.products }.flatMap{ it.toList() }
+    return allOrderedProducts.count{ it.equals(product) }
+
 }
