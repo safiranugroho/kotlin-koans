daeea8dec174cc1aec846224c48915fd2039c361
Safira Nugroho <snugroho@thoughtworks.com>
2018-09-13 11:48:39 +1000

    Done generic functionnn

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -20,11 +20,17 @@ fun task41(): Nothing = TODO(
 )
 
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+}
+
+fun <E, T : MutableCollection<E>, C : Collection<E>> C.partitionTo(first: T, second: T, predicate: (item: E) -> Boolean): Pair<T, T> {
+    for (item in this) {
+        val result = predicate(item)
+        if (result) first.add(item) else second.add(item)
+    }
+    return Pair(first, second)
 }
